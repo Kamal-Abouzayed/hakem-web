23bcb2eb55bc44fc7b3dc4f528555b71b0c9b245
Kamal-Abouzayed <kamal.abouzayed@gmail.com>
2023-04-29 00:54:29 +0300

    📦 NEW: Countries Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call(PermissionTableSeeder::class);
+        $this->call(CountriesTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(ContactTableSeeder::class);
         $this->call(MailListSeeder::class);
